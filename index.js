ecb15c895f3d943662886f85114ace8542b84ceb
keplib <keplib@gmail.com>
2023-04-24 19:39:47 +0200

    part3/3.3_solution

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people.</p><p>${today.toString()}</p>`);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === Number(id));
+  person ? res.json(person) : res.send('<p>Entry not found</p>');
+});
+
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
